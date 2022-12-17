8c501790227353151c22b73bcfeb8a7b999d6b73
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-12-17 23:11:58 +0100

    update v 1.0

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,16 +2,18 @@ import { TailSpin } from "react-loader-spinner";
 import style from './Loader.module.css';
 
 export const Loader = () => {
-    <div className={style.loader}>
-        <TailSpin
-            height="80"
-            width="80"
-            color="#000000"
-            ariaLabel="tail-spin-loading"
-            radius="1"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-        />
-    </div>
+    return (
+        <div className={style.loader}>
+            <TailSpin
+                height="80"
+                width="80"
+                color="#000000"
+                ariaLabel="tail-spin-loading"
+                radius="1"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+            />
+        </div>
+    )
 };
